8a0a0216669034ef9ac0ecddf67097a392aa7ffb
Peter Cline <pcline@crossbeam.com>
2022-11-27 09:43:08 -0600

    vterm-toggle and keymap updates

--- doom/custom.el
+++ doom/custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default))
- '(package-selected-packages '(import-popwin popwin clj-refactor cider)))
+ '(package-selected-packages
+   '(vterm-toggle vterm clj-refactor cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
